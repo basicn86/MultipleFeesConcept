286d88474941c4a4d15cebbf29ced74733977d1a
Nedim Basic <basicn86@gmail.com>
2023-12-24 20:20:45 -0500

    Added loan number in main window view model

--- MultipleFeesConcept/ViewModels/MainWindowViewModel.cs
+++ MultipleFeesConcept/ViewModels/MainWindowViewModel.cs
@@ -9,9 +9,37 @@ namespace MultipleFeesConcept.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private int _loanNumber;
+        public string LoanNumber
+        {
+            set
+            {
+                if (int.TryParse(value, out int temp))
+                {
+                    _loanNumber = temp;
+                }
+                else
+                {
+                    _loanNumber = 0;
+                }
+            }
+            get => _loanNumber.ToString();
+        }
+
         public MainWindowViewModel()
         {
-            
+
+            ShowDialog = new Interaction<FeesViewModel, MainWindowViewModel?>();
+
+            ShowFeesCommand = ReactiveCommand.CreateFromTask(async () =>
+            {
+                var feesViewModel = new FeesViewModel(0);
+                await ShowDialog.Handle(feesViewModel);
+            });
         }
+
+        public ICommand ShowFeesCommand { get; }
+
+        public Interaction<FeesViewModel, MainWindowViewModel?> ShowDialog { get; }
     }
 }
--- MultipleFeesConcept/Views/MainWindow.axaml.cs
+++ MultipleFeesConcept/Views/MainWindow.axaml.cs
@@ -1,12 +1,27 @@
 using Avalonia.Controls;
+using Avalonia.ReactiveUI;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using MultipleFeesConcept.ViewModels;
+using ReactiveUI;
+using System.Threading.Tasks;
 
 namespace MultipleFeesConcept.Views
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : ReactiveWindow<ViewModels.MainWindowViewModel>
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            this.WhenActivated(action => action(ViewModel!.ShowDialog.RegisterHandler(DoShowDialogAsync)));
+        }
+
+        private async Task DoShowDialogAsync(InteractionContext<FeesViewModel, MainWindowViewModel?> interaction)
+        {
+            var dialog = new FeesView();
+            dialog.DataContext = interaction.Input;
+            var result = await dialog.ShowDialog<MainWindowViewModel?>(this);
+            interaction.SetOutput(result);
         }
     }
 }
