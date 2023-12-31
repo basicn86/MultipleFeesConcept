bfbef628bf4c30aff1343dacf150711208a573cd
Nedim Basic <basicn86@gmail.com>
2023-12-30 22:13:08 -0500

    Added fee listbox

--- MultipleFeesConcept/ViewModels/FeesViewModel.cs
+++ MultipleFeesConcept/ViewModels/FeesViewModel.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MultipleFeesConcept.ViewModels
 {
+    public struct TestFee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
     public class FeesViewModel : ViewModelBase
     {
         public string loanAmount { get; }
+        public ObservableCollection<TestFee> fees { get; } = new ObservableCollection<TestFee> {
+            new TestFee { Id = 1, Name = "Potato" },
+            new TestFee { Id = 3, Name = "Apple" },
+            new TestFee { Id = 2, Name = "Orange" }
+        };
 
         public FeesViewModel(int _loanAmount)
         {
--- MultipleFeesConcept/Views/FeesView.axaml.cs
+++ MultipleFeesConcept/Views/FeesView.axaml.cs
@@ -1,10 +1,12 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using ReactiveUI;
 
 namespace MultipleFeesConcept.Views;
 
-public partial class FeesView : Window
+public partial class FeesView : ReactiveWindow<ViewModels.FeesViewModel>
 {
     public FeesView()
     {
